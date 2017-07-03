a30a781492e2456f81d23b294eba4dfcb8f9cf77
顾姗6092001913 <6092001913@zte.intra>
2017-07-03 17:06:17 +0800

    其他条件添加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,26 @@ export class AppComponent {
     {label: '7', closable: false}, {label: '8', closable: false}, {label: '9', closable: false},
     {label: '10', closable: false}, {label: '11', closable: false}, {label: '12', closable: false}];
 
-  quickChoices = [{label: '一天', id: '1'}, {label: '三天', id: '2'}, {label: '七天', id: '3'}]
+  quickChoices = [{label: '一天', id: '1'}, {label: '三天', id: '2'}, {label: '七天', id: '3'}];
 
-  selectedChoice = this.quickChoices[0]
+  selectedChoice = this.quickChoices[0];
 
+
+  businessTypes = [{label: 'LTE用户面', closable: false}, {label: 'LTE控制面', closable: false}];
+
+  displayTypes = [{label: '合并', id: '1'}, {label: '分表', id: '2'}];
+
+  displayType = this.displayTypes[0];
+
+  interfaces = [{label: 'S1-U', closable: false}, {label: 'S2-U', closable: false}];
+
+  userTypes = [{label: 'IMSI', closable: false}, {label: 'MSISDN', closable: false}];
+
+  // TODO fix#77
+  // selectUserType = [this.userTypes[0]];
+  selectUserType = new ArrayCollection([{label: 'IMSI', closable: false}]);
+
+  maxRecord = 1000;
 
   constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
   }
@@ -44,10 +60,15 @@ export class AppComponent {
     this.handleChange();
   }
 
-  handleChange(event?){
+  handleChange() {
     this.rangeTimeComboValue = new ArrayCollection([
       {label: TimeService.getFormatDate(this.beginDate, TimeGr.date), closable: false},
       {label: TimeService.getFormatDate(this.endDate, TimeGr.date), closable: false}
     ]);
   }
+
+  displayTypeChange(displayType) {
+    console.log(displayType)
+  }
+
 }
