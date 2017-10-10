29ac9dd6e0d03fc6a8c820ccc53762da7e0bf97c
陈旭10045812 <chen.xu8@zte.com.cn>
2017-10-10 14:50:31 +0800

    upgrade package versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {JigsawModule} from '@rdkmaster/jigsaw';
@@ -14,7 +13,7 @@ import {AppComponent} from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpModule, BrowserAnimationsModule,
+    BrowserModule, FormsModule, BrowserAnimationsModule,
     JigsawModule, TranslateModule.forRoot()
   ],
   providers: [TranslateService],
