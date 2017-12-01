6fe49f9e1adc739f2a414c04295cbd66ddfd36af
陈旭10045812 <chen.xu8@zte.com.cn>
2017-12-01 18:03:04 +0800

    indent change to 4 spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import {Component, Renderer2, ViewContainerRef} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  title = 'Jigsaw Seed';
+    title = 'Jigsaw Seed';
 
-  constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
-  }
+    constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
+    }
 
-  gotoTourist() {
-    window.open('https://github.com/rdkmaster/jigsaw/blob/master/docs/tourist/index.md', '_blank');
-  }
+    gotoTourist() {
+        window.open('https://github.com/rdkmaster/jigsaw/blob/master/docs/tourist/index.md', '_blank');
+    }
 
-  gotoGithub() {
-    window.open('https://github.com/rdkmaster/jigsaw', '_blank');
-  }
+    gotoGithub() {
+        window.open('https://github.com/rdkmaster/jigsaw', '_blank');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,30 +9,30 @@ import {AppComponent} from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule, BrowserAnimationsModule,
-    JigsawModule, TranslateModule.forRoot()
-  ],
-  providers: [TranslateService],
-  bootstrap: [AppComponent],
-  entryComponents: []
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule, FormsModule, BrowserAnimationsModule,
+        JigsawModule, TranslateModule.forRoot()
+    ],
+    providers: [TranslateService],
+    bootstrap: [AppComponent],
+    entryComponents: []
 })
 export class AppModule {
-  constructor(translateService: TranslateService) {
-    translateService.setTranslation('zh', {
-      'get-started': '马上开始',
-      'give-star': '给 Jigsaw 点个星星'
-    });
-    translateService.setTranslation('en', {
-      'get-started': 'Get started',
-      'give-star': 'Give us a star on Github.com'
-    });
+    constructor(translateService: TranslateService) {
+        translateService.setTranslation('zh', {
+            'get-started': '马上开始',
+            'give-star': '给 Jigsaw 点个星星'
+        });
+        translateService.setTranslation('en', {
+            'get-started': 'Get started',
+            'give-star': 'Give us a star on Github.com'
+        });
 
-    const lang: string = translateService.getBrowserLang();
-    translateService.setDefaultLang(lang);
-    translateService.use(lang);
-  }
+        const lang: string = translateService.getBrowserLang();
+        translateService.setDefaultLang(lang);
+        translateService.use(lang);
+    }
 }
