391d0ebb1b249ab6a1179536844016b2a9aaf3e3
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-07-04 11:56:08 +0800

    update file to ng8

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule, { preserveWhitespaces: true });
+
